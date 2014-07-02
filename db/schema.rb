95980a8a7594641eaee810573d1e7e8f0be532a1
Nelly Lam <nellyannlam@gmail.com>
2014-07-01 20:28:28 -0700

    Generated user scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140604032713) do
+ActiveRecord::Schema.define(version: 20140702032254) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
